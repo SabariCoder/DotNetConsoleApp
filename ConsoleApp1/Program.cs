847d1a65add6036117ae2bd06b8b0378158443b0
Sabarinathan T <sabari.delphi@gmail.com>
2023-11-28 20:54:24 +0530

    test

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/*
+using System;
+
+namespace Hello { 
+    class Program
+    {
+        static void Main() {
+            Console.WriteLine(DateTime.Now.Date.ToString());
+            Console.WriteLine(DateTime.Now.ToLongTimeString());
+            Console.WriteLine("Hello World");
+
+            string str1 = Console.ReadLine();
+            Console.WriteLine(str1);
+            
+            Console.ReadKey();
+        }
+
+    }
+
+}
+*/
